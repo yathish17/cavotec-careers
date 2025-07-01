d7b21d6517a1ac1df69dc13376a8a6367b718f30
Yathish Shirali <Yathish.shirali@cavotec.com>
2025-07-01 11:41:56 +0530

    Dyanamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -32,13 +32,21 @@ app = Flask(__name__)
 def hello():
     jobs = load_jobs_from_db()
     return render_template("home.html", 
-                         jobs=jobs, 
-                         company_name='Cavotec')
+                         jobs=jobs
+                        )
 
 @app.route("/api/jobs")
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+   job = load_job_from_db(id)
+   if not job:
+      return "Not Found", 404
+   
+   return render_template('jobpage.html', job = job)
 
 
 
